15af865361dc6e1ce7f4bfc4afcd996f6764dcd8
Melvine Mathews <melvinis@gmail.com>
2020-08-30 20:18:30 +0400

    tenth commit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -32,7 +32,7 @@ class Portfolio extends Component {
 
           <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Some samples of my continued tinkering with Webapps, APIs, Artificial Intelligence, Cloud Computing, Machine Learning, Blockchains.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
               {projects}
